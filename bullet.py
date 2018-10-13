065be82fdd42425b3bbe8f7e428b0a24ee95f061
Lukas Knapek <lukas.knapek@kolabnow.com>
2018-10-13 18:58:49 +0100

    Flying enemy shooting and collisions, drawing refactor

--- bullet.py
+++ bullet.py
@@ -5,21 +5,11 @@ class Bullet(pygame.sprite.Sprite):
     def __init__(self, width, height):
         super().__init__()
 
-        self.change_size(width, height)
-
         self.speed = 10
         self.direction = 'right'
-
         self.image = pygame.image.load("Assets/Sprites/bullet.png").convert_alpha()
-
         self.rect = self.image.get_rect()
 
-    def change_size(self, width, height):
-        self.width = width
-        self.height = height
-        self.image = pygame.Surface([width, height])
-        self.image.fill(WHITE)
-        self.image.set_colorkey(WHITE)
 
     def update(self):
         self.move()
--- flying_zombie.py
+++ flying_zombie.py
@@ -1,5 +1,7 @@
-import pygame, os
+import pygame, os, random
 from enemy import Enemy
+from bullet import Bullet
+from drawing_manager import *
 
 
 class FlyingZombie(Enemy):
@@ -11,8 +13,24 @@ class FlyingZombie(Enemy):
 
         self.move_sprites = [pygame.image.load(img) for img in sprites_paths]
         self.animation_counter = 0
+        self.can_shoot = False
+        self.spitting_interval = random.randint(2000, 3500)
+
 
     def move(self):
         self.image = self.move_sprites[(self.animation_counter // 10) % len(self.move_sprites)]
         self.animation_counter += 1
         super().move()
+
+        if self.can_shoot:
+            self.shoot()
+
+    def shoot(self):
+        if self.can_shoot:
+            bullet = Bullet(64, 64)
+            bullet.rect.x = self.rect.x
+            bullet.rect.y = self.rect.y
+            bullet.direction = "down"
+            spit_sprites.add(bullet)
+
+            self.can_shoot = False
